53996f8182550e8821fa3f506e82a18e784b2138
Yi Zhang <zhangy@amss.ac.cn>
2017-12-29 10:59:16 +0100

    add some files to ec1

---
+++ ec1/ec1_ann.m
@@ -0,0 +1,50 @@
+{OrePolynomial[{{1 + s^2 + 2*t^2 + 2*s^2*t^2 + t^4 + s^2*t^4, {0, 0, 0, 1}}, 
+   {-4*b*m11*s*s1 + 2*b*m21*s2 - 4*d*m22*s*s2 - 2*b*m21*s^2*s2 + 2*t + 
+     2*s^2*t + 4*d*m11*s1*t - 4*d*m11*s^2*s1*t + 4*b*m22*s2*t + 
+     8*d*m21*s*s2*t - 4*b*m22*s^2*s2*t + 4*b*m11*s*s1*t^2 - 2*b*m21*s2*t^2 + 
+     4*d*m22*s*s2*t^2 + 2*b*m21*s^2*s2*t^2 + 2*t^3 + 2*s^2*t^3, 
+    {0, 0, 0, 0}}}, OreAlgebraObject[{Der[d], Der[b], Der[s], Der[t]}, 
+   Expand, #1 + #2 & , Expand[#1*#2] & , None], DegreeLexicographic], 
+ OrePolynomial[{{1 + 3*s^2 + 3*s^4 + s^6 + t^2 + 3*s^2*t^2 + 3*s^4*t^2 + 
+     s^6*t^2, {0, 0, 1, 0}}, {2*s + 4*s^3 + 2*s^5 + 4*b^2*s*s1 - 4*d^2*s*s1 + 
+     4*d*m11*s*s1 - 4*b^2*s^3*s1 + 4*d^2*s^3*s1 + 4*d*m11*s^3*s1 - 
+     2*d*m21*s2 - 4*b^2*s*s2 + 4*d^2*s*s2 + 4*b*m22*s*s2 + 4*b^2*s^3*s2 - 
+     4*d^2*s^3*s2 + 4*b*m22*s^3*s2 + 2*d*m21*s^4*s2 - 4*b*m11*s1*t + 
+     4*b*m11*s^4*s1*t - 4*d*m22*s2*t - 8*b*m21*s*s2*t - 8*b*m21*s^3*s2*t + 
+     4*d*m22*s^4*s2*t + 2*s*t^2 + 4*s^3*t^2 + 2*s^5*t^2 + 4*b^2*s*s1*t^2 - 
+     4*d^2*s*s1*t^2 - 4*d*m11*s*s1*t^2 - 4*b^2*s^3*s1*t^2 + 
+     4*d^2*s^3*s1*t^2 - 4*d*m11*s^3*s1*t^2 + 2*d*m21*s2*t^2 - 
+     4*b^2*s*s2*t^2 + 4*d^2*s*s2*t^2 - 4*b*m22*s*s2*t^2 + 4*b^2*s^3*s2*t^2 - 
+     4*d^2*s^3*s2*t^2 - 4*b*m22*s^3*s2*t^2 - 2*d*m21*s^4*s2*t^2, 
+    {0, 0, 0, 0}}}, OreAlgebraObject[{Der[d], Der[b], Der[s], Der[t]}, 
+   Expand, #1 + #2 & , Expand[#1*#2] & , None], DegreeLexicographic], 
+ OrePolynomial[{{b^2 - d^2 + 2*b^2*s^2 - 2*d^2*s^2 + b^2*s^4 - d^2*s^4 + 
+     b^2*t^2 - d^2*t^2 + 2*b^2*s^2*t^2 - 2*d^2*s^2*t^2 + b^2*s^4*t^2 - 
+     d^2*s^4*t^2, {0, 1, 0, 0}}, {-2*b - 4*b*s^2 - 2*b*s^4 + 4*b^3*s^2*s1 - 
+     4*b*d^2*s^2*s1 + b^3*s2 - b*d^2*s2 - b^2*m22*s2 + d^2*m22*s2 - 
+     2*b^3*s^2*s2 + 2*b*d^2*s^2*s2 + b^3*s^4*s2 - b*d^2*s^4*s2 + 
+     b^2*m22*s^4*s2 - d^2*m22*s^4*s2 - 4*b^2*m11*s*s1*t + 4*d^2*m11*s*s1*t - 
+     4*b^2*m11*s^3*s1*t + 4*d^2*m11*s^3*s1*t + 2*b^2*m21*s2*t - 
+     2*d^2*m21*s2*t - 2*b^2*m21*s^4*s2*t + 2*d^2*m21*s^4*s2*t - 2*b*t^2 - 
+     4*b*s^2*t^2 - 2*b*s^4*t^2 + 4*b^3*s^2*s1*t^2 - 4*b*d^2*s^2*s1*t^2 + 
+     b^3*s2*t^2 - b*d^2*s2*t^2 + b^2*m22*s2*t^2 - d^2*m22*s2*t^2 - 
+     2*b^3*s^2*s2*t^2 + 2*b*d^2*s^2*s2*t^2 + b^3*s^4*s2*t^2 - 
+     b*d^2*s^4*s2*t^2 - b^2*m22*s^4*s2*t^2 + d^2*m22*s^4*s2*t^2, 
+    {0, 0, 0, 0}}}, OreAlgebraObject[{Der[d], Der[b], Der[s], Der[t]}, 
+   Expand, #1 + #2 & , Expand[#1*#2] & , None], DegreeLexicographic], 
+ OrePolynomial[{{-b^2 + d^2 - 2*b^2*s^2 + 2*d^2*s^2 - b^2*s^4 + d^2*s^4 - 
+     b^2*t^2 + d^2*t^2 - 2*b^2*s^2*t^2 + 2*d^2*s^2*t^2 - b^2*s^4*t^2 + 
+     d^2*s^4*t^2, {1, 0, 0, 0}}, {-2*d - 4*d*s^2 - 2*d*s^4 - b^2*d*s1 + 
+     d^3*s1 + b^2*m11*s1 - d^2*m11*s1 + 2*b^2*d*s^2*s1 - 2*d^3*s^2*s1 - 
+     b^2*d*s^4*s1 + d^3*s^4*s1 - b^2*m11*s^4*s1 + d^2*m11*s^4*s1 + 
+     2*b^2*m21*s*s2 - 2*d^2*m21*s*s2 - 4*b^2*d*s^2*s2 + 4*d^3*s^2*s2 + 
+     2*b^2*m21*s^3*s2 - 2*d^2*m21*s^3*s2 + 4*b^2*m22*s*s2*t - 
+     4*d^2*m22*s*s2*t + 4*b^2*m22*s^3*s2*t - 4*d^2*m22*s^3*s2*t - 2*d*t^2 - 
+     4*d*s^2*t^2 - 2*d*s^4*t^2 - b^2*d*s1*t^2 + d^3*s1*t^2 - b^2*m11*s1*t^2 + 
+     d^2*m11*s1*t^2 + 2*b^2*d*s^2*s1*t^2 - 2*d^3*s^2*s1*t^2 - 
+     b^2*d*s^4*s1*t^2 + d^3*s^4*s1*t^2 + b^2*m11*s^4*s1*t^2 - 
+     d^2*m11*s^4*s1*t^2 - 2*b^2*m21*s*s2*t^2 + 2*d^2*m21*s*s2*t^2 - 
+     4*b^2*d*s^2*s2*t^2 + 4*d^3*s^2*s2*t^2 - 2*b^2*m21*s^3*s2*t^2 + 
+     2*d^2*m21*s^3*s2*t^2, {0, 0, 0, 0}}}, 
+  OreAlgebraObject[{Der[d], Der[b], Der[s], Der[t]}, Expand, #1 + #2 & , 
+   Expand[#1*#2] & , None], DegreeLexicographic]}
